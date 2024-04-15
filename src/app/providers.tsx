3b8d48c29ac14fde00a78513cf66e6366fd40400
Solpio <ssusolpio@gmail.com>
2024-04-16 01:20:13 +0400

    fix manifest

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<TonConnectUIProvider
-			manifestUrl={`http://localhost:3000/tonconnect-manifest.json`} //TODO: fix URL
+			manifestUrl={`http://localhost:3000/manifest.json`} //TODO: fix URL
 		>
 			{children}
 		</TonConnectUIProvider>
