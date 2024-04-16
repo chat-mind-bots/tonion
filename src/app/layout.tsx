6cb6a9a169710bc7e379e2e95fb9c273c283357a
Solpio <ssusolpio@gmail.com>
2024-04-17 03:15:35 +0400

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 import { Providers } from "@/app/providers";
 import clsx from "clsx";
 
